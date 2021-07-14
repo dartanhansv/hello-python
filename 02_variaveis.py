5856b8fbd9c6a1b84d7b3c5f970c863f04e1b734
Dartanhan S. Venturini <dartanhan@outlook.com>
2021-07-13 21:36:28 -0300

    Add vars

---
+++ 02_variaveis.py
@@ -0,0 +1,43 @@
+'''
+No Python diferente da maioria das demais linguagens, não há necessidade de declarar o tipo da variável,
+basta atribuir o valor com simbolo "="
+'''
+
+# Exemplos de Variáveis
+
+# Tipo string - Texto
+nome = "D'Artagnan"
+profissao = 'Mosqueteiro'
+
+
+# Tipo float
+altura = 1.80
+
+# Tipo inteiro
+idade = 32
+
+# Tipo complex - estatística
+complexo = 1+3j
+
+# Tipo bool - Booleano
+casado = True
+filhos = False
+
+
+# LISTAS
+#Tipo Array
+equipe = ['Athos', 'Porthos']
+
+# Operações com arrays
+equipe.append('Aramis') # Adiciona um objeto na última posição do Array
+equipe.insert(0, 'Tréville') # Insere um objeto em uma posição específica do Array
+# a primeira posição do array é o Zero
+
+equipe.append('Tréville')
+
+# Remove um objeto no array pelo nome
+equipe.remove('Tréville')
+
+# Remove um objeto no array pela posição
+equipe.pop(3)
+
