10e117b614a2986515d40bd919da4e66fd33fb49
Dartanhan S. Venturini <dartanhan@outlook.com>
2021-07-15 22:55:43 -0300

    Add dict

--- 02_variaveis.py
+++ 02_variaveis.py
@@ -41,3 +41,21 @@ equipe.remove('Tréville')
 # Remove um objeto no array pela posição
 equipe.pop(3)
 
+
+
+# Listas tipo chave:valor - dict(dicionário)
+
+livros = {}
+
+livros['titulo'] = 'A Arte da Prudência'
+livros['ano'] = 1647
+
+pessoa = {'nome':'José da Silva', 'idade':30, 'altura': 1.8, 'peso': 93}
+'''
+ Os métodos do dicionário são parecidos com uma função, 
+ possuem parametros e retornam valores, mas a sintaxe é diferente.
+'''
+
+pessoa.keys() # Retorna a lista com os índices
+
+pessoa.values() # Retorna a lista com os valores
