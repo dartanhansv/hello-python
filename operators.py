6b463159c8436538c607c5b3ed9a51cb1b5b0fcd
Dartanhan S. Venturini <dartanhan@outlook.com>
2021-07-18 16:17:14 -0300

    Add Basic Operators

---
+++ operators.py
@@ -0,0 +1,39 @@
+
+# Operadores
+
+# de Comparação
+'''
+==  Equal
+!=  Not equal
+>   Greater than
+<   Less than
+>=  Greater than or equal to
+<=  Less than or equal to
+'''
+
+
+# de Atribuição - assignment operators
+'''
+Atribuir valor:
+x = 10
+
+Somar 5 ou valor existente - "modo tradicional"
+x = x + 5
+
+Outra forma de alterar valor existente
+x += 5
+x -= 5
+x *= 5
+x /= 5
+
+"O resto da divisão"
+x %= 5
+
+
+'''
+
+x = 10
+y = 3
+z = x % y
+w = f' o resto de {x}/{y} é {z}'
+print(w)
