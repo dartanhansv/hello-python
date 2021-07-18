103170f4b3b6ca7b05a3128a8ecbdac5d822bdc7
Dartanhan S. Venturini <dartanhan@outlook.com>
2021-07-18 16:44:57 -0300

    if/else

--- operators.py
+++ operators.py
@@ -1,7 +1,7 @@
 
 # Operadores
 
-# de Comparação
+# Comparison
 '''
 ==  Equal
 !=  Not equal
@@ -9,11 +9,11 @@
 <   Less than
 >=  Greater than or equal to
 <=  Less than or equal to
-'''
 
 
-# de Atribuição - assignment operators
-'''
+
+# Assignment
+
 Atribuir valor:
 x = 10
 
@@ -30,10 +30,46 @@ x /= 5
 x %= 5
 
 
-'''
 
 x = 10
 y = 3
 z = x % y
 w = f' o resto de {x}/{y} é {z}'
 print(w)
+
+
+
+# Conditional Statements if/else
+velocidade = float(input('Velocidade atual: '))
+
+if velocidade > 110:
+    print('Acima da Velocidade Permitida!')
+    print("Favor Reduzir a sua velocidade!")
+elif velocidade < 60:
+    print('Favor dirigir acima de 80km/h!')
+else:
+    print('Velocidade OK!')
+
+
+
+# Logical  and, or, not
+# 1. and
+renda_acima_5mil = True
+nome_limpo = True
+
+if renda_acima_5mil and nome_limpo:
+    print('Financiamento Aprovado')
+else:
+    print('Financiamento Negado')
+
+
+# 2. or
+renda_acima_5mil = False
+nome_limpo = True
+
+if renda_acima_5mil or nome_limpo:
+    print('Financiamento Aprovado')
+else:
+    print('Financiamento Negado')
+'''
+
