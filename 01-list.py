1d80734aa6cff68c25283c325b8ebad1e1f69ad5
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-04-29 22:50:19 -0300

    list concepts

--- 01-list.py
+++ 01-list.py
@@ -1,3 +1,21 @@
 '''
 Lists in Python are containers that can store anything you want
+Python list is called an array in other languages
 '''
+
+# An empty list
+fears = []
+
+# List of numbers
+lottery = [3, 15, 53, 55, 62, 74, 75]
+
+# List of strings
+cities = ["Blue Valley", "Evergreen City", "Gotham City", "Smallville"]
+
+# List of mixed items
+both = [3, "Gotham", 15, "Smmallville", 10.5]
+
+print("I'm afraid of", fears)
+print("My lottery ticket: " ,lottery)
+print("The cities are: ", cities)
+print(both )
