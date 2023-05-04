ff0097a5bd4393abd87d30d143151819ae9f4b9d
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-05-04 15:03:16 -0300

    add **kwargs examples

--- args_kwargs.py
+++ args_kwargs.py
@@ -18,3 +18,26 @@ print('\n')
 
 function_A('A','B','C', 'D', 'E','F')
 print('\n')
+
+
+'''
+kwargs stands for keyword arguments
+The only difference from args is that it uses keywords and returns the values in the form of a Dictionary
+'''
+
+
+
+def function_B(**kwargs):
+    print(kwargs)
+
+function_B(First='Dartanhan', Middle='S.', Last='Venturini')
+
+
+print('\n')
+def function_C(**kwargs):
+    for key, value in kwargs.items():
+        #print(key)
+        #print(value)
+        print("The value of {} is {}" .format(key, value))
+
+function_C(First_name='Dartanhan', Middle_name='S.', Last_name='Venturini')
