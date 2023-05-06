3b980113be5867663dd0de08d650b7066c9fd1b2
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-05-06 18:09:06 -0300

    simple try/except

--- 40-exceptions.py
+++ 40-exceptions.py
@@ -3,6 +3,15 @@ If we try to open a file that doens't exist, we got an error and the program cra
 To avoid that, we use try and except
 '''
 
+'''
 # 1st, the FileNotFoundError
 file = open('wrong_file_name.txt', 'r')
 print(file)
+'''
+
+# 2nd, using try/except to prevent the program from failing
+try:
+    file = open('wrong_file_name.txt', 'r')
+    print(file)
+except:
+    print("File doesn't exist")
