cf91dc10c7ccb79b2eb55008b039dc87c952f941
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-05-12 21:48:34 -0300

    add dict-copy

---
+++ 21-dict-copy.py
@@ -0,0 +1,7 @@
+
+
+url = dict(Google="https://www.google.com/", Cisco="https://www.cisco.com/", Quora="https://www.quora.com/", Reddit="https://www.reddit.com/"  )
+websites = url.copy()
+print('\n', websites, '\n')
+
+
