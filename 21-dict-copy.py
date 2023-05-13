a399ee98f10153d2793a76e3be83e9518fd2765c
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-05-12 21:52:13 -0300

    add example

--- 21-dict-copy.py
+++ 21-dict-copy.py
@@ -5,3 +5,6 @@ websites = url.copy()
 print('\n', websites, '\n')
 
 
+links = [('Google',"https://www.google.com/"), ('Cisco', "https://www.cisco.com/"), ('Quora', "https://www.quora.com/"), ('Reddit', "https://www.reddit.com/"  )]
+webpages = dict(links)
+print('\n', webpages, '\n')
